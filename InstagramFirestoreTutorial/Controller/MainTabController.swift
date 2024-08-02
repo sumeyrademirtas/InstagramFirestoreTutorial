b1d2ac20c519adb703755fcd3fd0d49d9cca99f0
Sumeyra Demirtas <smyrdmrts@gmail.com>
2024-08-02 10:52:04 +0300

    Initial Commit

---
+++ InstagramFirestoreTutorial/Controller/MainTabController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by Sümeyra Demirtaş on 2.08.2024.
+//
+
+import UIKit
+
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
---
+++ InstagramFirestoreTutorial/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by Sümeyra Demirtaş on 2.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .blue
+    }
+
+
+}
+
