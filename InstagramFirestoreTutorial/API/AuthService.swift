a05892d96e456c2a20179c15b92bc6d4f6276a55
Sumeyra Demirtas <smyrdmrts@gmail.com>
2024-08-07 10:32:19 +0300

    feat(auth): integrate user registration with Firebase and Firestore
    # implemented registerUser function in AuthService to handle user registration.
    # added image upload functionality to store user profile images in Firebase Storage.
    # updated RegistrationController to handle user sign-up process and error handling.

--- InstagramFirestoreTutorial/API/AuthService.swift
+++ InstagramFirestoreTutorial/API/AuthService.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Firebase
+import FirebaseAuth
 
 
 struct AuthCredentials {
@@ -18,7 +19,23 @@ struct AuthCredentials {
 }
 
 struct AuthService {
-    static func registerUser(withCredential credentials: AuthCredentials) {
-        print("DEBUG: Credentials are \(credentials)")
+    static func registerUser(withCredential credentials: AuthCredentials, completion: @escaping(Error?)-> Void) {
+        
+        ImageUploader.uploadImage(image: credentials.profileImage) { imageUrl in
+            Auth.auth().createUser(withEmail: credentials.email, password: credentials.password) { (result, error) in
+                
+                if let error = error {
+                    print("DEBUG: Failed to register user \(error.localizedDescription)")
+                    return
+                }
+                
+                guard let uid = result?.user.uid else { return }
+                
+                let data: [String: Any] = ["email": credentials.email, "fullname": credentials.fullname, "profileImageUrl": imageUrl, "uid": uid, "username": credentials.username]
+                
+                
+                Firestore.firestore().collection("users").document(uid).setData(data, completion: completion)
+            }
+        }
     }
 }
